f8e418aabd1abb75befae476f0b636f7398ed789
Nasim Jamaly <mamnasimjamaly@yahoo.com>
2016-08-06 21:46:03 -0700

    added markup and logic for question display and flow from one question to the other

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -9,5 +9,40 @@
 			var vm = this;
 			vm.quizMetrics = quizMetrics;
 			vm.dataService = DataService;
+			vm.activeQuestion = 0;
+
+			var numQuestionsAnswered = 0;
+			vm.questionAnswered = questionAnswered;
+			vm.setActiveQuestion = setActiveQuestion;
+
+			function setActiveQuestion() {
+				var breakOut = false;
+				var quizLength = DataService.quizQuestions.length - 1;
+
+				while(!breakOut) {
+					//vm.activeQuestion = vm.activeQuestion < quizLength ? vm.activeQuestion++ : 0;
+					if(vm.activeQuestion < quizLength) {
+						vm.activeQuestion++;
+					} else {
+						vm.activeQuestion = 0;
+					}
+					if(DataService.quizQuestions[vm.activeQuestion].selected == null) {
+					breakOut = true;
+					}
+				}
+
+				
+			}
+
+			function questionAnswered() {
+				var length = DataService.quizQuestions.length;
+				if(DataService.quizQuestions[vm.activeQuestion].selected !== null) {
+					numQuestionsAnswered++;
+					if(numQuestionsAnswered >= length) {
+						//finaize quiz
+					}
+				}
+				vm.setActiveQuestion();
+			}
 		}
 })();
